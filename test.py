83e362a87e599450d2f558bbe63a0d9c4499f7ea
Rupa Shankar <rupsshankar@gmail.com>
2013-10-01 12:10:57 -0400

    Attempting to edit Wikidata pages

--- test.py
+++ test.py
@@ -1,11 +1,43 @@
-import mwclient
+import mwclient, json
+import MWApi
 
-def test(): 
+username = 'rupss'
+password = 'creekview'
+
+def test_get_wikipedia_page_data(): 
 	site = mwclient.Site('en.wikipedia.org')
 	page = site.Pages['Wakka_Wakka']
 	text = page.edit()
 	print text.encode('utf-8')
 	print 'hello world'
 
+def test_get_wikidata_page_data(): 
+	site = mwclient.Site('test.wikidata.org')
+	site.login(username, password)
+	page = site.Pages['Q80']
+	text = page.edit()
+	print text.encode('utf-8')
+	print '--------'
+	obj = json.loads(text.encode('utf-8'))
+	obj['description']['en'] = 'Successful change'
+	print '---------'
+	print type(json.dumps(obj))
+	page.save(json.dumps(obj), 'Test change')
+
+def sample(): 
+	site = mwclient.Site('commons.wikimedia.org')
+	site.login(username, password)  # Optional
+
+	# Edit page
+	page = site.Pages['Commons:Sandbox']
+	text = page.edit()
+	print 'Text in sandbox:', text.encode('utf-8')
+	page.save(text + u'\nExtra data TEST', summary = 'Test edit')
+
+def test_mwapi(): 
+	obj = MWAPI('https://en.wikipedia.org')
+
 if __name__ == "__main__": 
-	test()
+	#test_get_wikidata_page_data()
+	#sample()
+	test_mwapi()
